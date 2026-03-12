bce46e14a6f71d9d6464b614b0a0f12d8ade024f
dnginnovator <kasscns@gmail.com>
2026-03-12 22:34:42 +0800

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -41,4 +41,6 @@ def home():
     return 'AI Study Time Recommendation Flask Backend'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
